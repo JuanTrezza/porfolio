29283c749cbe6192a7c2abb7e5421bef72d0a75e
JuanTrezza <tuemail@gmail.com>
2026-06-08 22:30:31 -0300

    fix: configurar publicDir para servir CV PDF

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     base: '/porfolio/',
+    publicDir: 'public',
     plugins: [react()],
     resolve: {
       alias: {
